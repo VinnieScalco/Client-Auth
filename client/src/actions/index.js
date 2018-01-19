fe02044db521ac0ab1d0f9e0437a3e543a152a61
Vinnie <vinnie.scalco@gmail.com>
2018-01-19 08:14:16 -0600

    everything working..

---
+++ client/src/actions/index.js
@@ -0,0 +1,88 @@
+import axios from 'axios';
+// Fixes an issue with axios and express-session where sessions
+// would not persist between routes
+axios.defaults.withCredentials = true;
+const ROOT_URL = 'http://localhost:5000/api';
+
+export const USER_REGISTERED = 'USER_REGISTERED';
+export const USER_AUTHENTICATED = 'USER_AUTHENTICATED';
+export const USER_UNAUTHENTICATED = 'USER_UNAUTHENTICATED';
+export const AUTHENTICATION_ERROR = 'AUTHENTICATION_ERROR';
+export const GET_USERS = 'GET_USERS';
+export const CHECK_IF_AUTHENTICATED = 'CHECK_IF_AUTHENTICATED';
+
+export const authError = error => {
+  return {
+    type: AUTHENTICATION_ERROR,
+    payload: error
+  };
+};
+
+export const register = (username, password, confirmPassword, history) => {
+  return dispatch => {
+    if (password !== confirmPassword) {
+      dispatch(authError('Passwords do not match'));
+      return;
+    }
+    axios
+      .post(`${ROOT_URL}/users`, { username, password })
+      .then(() => {
+        dispatch({
+          type: USER_REGISTERED
+        });
+        history.push('/signin');
+      })
+      .catch(() => {
+        console.log('failed');
+        dispatch(authError('Failed to register user'));
+      });
+  };
+};
+
+export const login = (username, password, history) => {
+  return dispatch => {
+    axios
+      .post(`${ROOT_URL}/login`, { username, password })
+      .then((response) => {
+        const token = response.data.token;
+        localStorage.setItem('token', token);
+        dispatch({
+          type: USER_AUTHENTICATED
+        });
+        history.push('/users');
+      })
+      .catch(() => {
+        dispatch(authError('Incorrect email/password combo'));
+      });
+  };
+};
+
+export const logout = () => {
+  return dispatch => {
+    localStorage.removeItem('token');
+    dispatch({
+      type: USER_UNAUTHENTICATED
+    });
+  };
+};
+
+export const getUsers = () => {
+  const token = localStorage.getItem('token');
+  return dispatch => {
+    axios
+      .get(`${ROOT_URL}/users`, { 
+        headers: {
+          Authorization: token
+        }
+      })
+      .then(response => {
+        dispatch({
+          type: GET_USERS,
+          payload: response.data
+        });
+      })
+      .catch(() => {
+        dispatch(authError('Failed to fetch users'));
+      });
+  };
+};
